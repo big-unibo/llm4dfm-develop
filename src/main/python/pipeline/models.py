b0ec01ac2cf7edc29a744786e7ec45b51438a6bb
Luca Rubboli <luca.rubboli2@studio.unibo.it>
2024-06-05 11:15:47 +0200

    feat: add the save model once imported, refactor methods from utils to models

--- src/main/python/pipeline/models.py
+++ src/main/python/pipeline/models.py
@@ -1,5 +1,11 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 from torch import bfloat16
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+save_directory = os.getenv('SAVE_MODELS')
 
 
 def load_model_and_tokenizer(model_name, key, quantization):
@@ -18,13 +24,69 @@ def load_model_and_tokenizer(model_name, key, quantization):
             m_name = 'mistralai/Mistral-7B-Instruct-v0.1',
         case _:
             m_name = ''
-    return (AutoModelForCausalLM.from_pretrained(m_name,
-                                                 # trust_remote_code=True,
-                                                 # quantization_config=bnb_config,
-                                                 # device_map='auto',
-                                                 # token=key
-                                                 ),
-            AutoTokenizer.from_pretrained(m_name,
-                                          # token=key
-                                          )
-            )
+
+    if not os.path.exists(save_directory):
+        os.makedirs(save_directory)
+
+    model_directory = save_directory + m_name.replace(" ", "-").replace("/", "-") + '/'
+    model_save_name = 'pytorch_model.bin'
+    tokenizer_save_name = 'tokenizer_config.json'
+
+    # Check if the saved model directory already contains the model files
+    model_files_exist = os.path.exists(os.path.join(model_directory, model_save_name))
+    tokenizer_files_exist = os.path.exists(os.path.join(model_directory, tokenizer_save_name))
+
+    if model_files_exist and tokenizer_files_exist:
+        # Load the model and tokenizer from the saved directory
+        model = AutoModelForCausalLM.from_pretrained(model_directory)
+        tokenizer = AutoTokenizer.from_pretrained(model_directory)
+    else:
+        # Download and load the model and tokenizer from Hugging Face
+        model = AutoModelForCausalLM.from_pretrained(m_name,
+                                                     # trust_remote_code=True,
+                                                     # quantization_config=bnb_config,
+                                                     # device_map='auto',
+                                                     # token=key
+                                                     )
+        tokenizer = AutoTokenizer.from_pretrained(m_name,
+                                                  # token=key
+                                                  )
+
+        # Save the model and tokenizer
+        model.save_pretrained(model_directory)
+        tokenizer.save_pretrained(model_directory)
+
+    return model, tokenizer
+
+
+# compute a batch given a model, a tokenizer and input_text, returning results
+def model_import_batch(model, tokenizer, text) -> str:
+    encoded = tokenizer.apply_chat_template(text, return_tensors="pt")
+
+    generated_ids = model.generate(encoded, max_new_tokens=1000, do_sample=True)
+    decoded = tokenizer.batch_decode(generated_ids)
+
+    # with torch.no_grad():
+    #     model_outputs = model.generate(**text_to_use)
+
+    # generated_text = tokenizer.decode(model_outputs[0], skip_special_tokens=True)
+
+    return decoded
+
+
+# compute a batch given apis and configurations
+def model_api_batch(openai, config, text) -> str:
+    response = openai.ChatCompletion.create(
+        model=config['name'],
+        messages=[
+            {'role': config['role'], 'content': text},  # TODO check if role should be inserted in input
+        ],
+        max_tokens=config['max_tokens'],
+        n=config['n_responses'],
+        stop=config['stop'],
+        temperature=config['temperature'],
+    )
+
+    output_message = response['choices'][0]['message']['content']  # TODO works only with gpt ?
+
+    return output_message
