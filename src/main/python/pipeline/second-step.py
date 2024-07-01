ba590655ccaf223566f50766737d8288b913e696
Luca Rubboli <luca.rubboli@libero.it>
2024-07-01 16:02:15 +0200

    Feat: add graph visualization

--- src/main/python/pipeline/second-step.py
+++ src/main/python/pipeline/second-step.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from utils import load_yaml, load_ground_truth_exercise, load_output_exercise
-from ssutils import clean_gt_dependencies
+from ssutils import clean_gt_dependencies, remove_explicit_tables_to_output, is_a_valid_role_dependency
+import networkx as nx
+import matplotlib.pyplot as plt
 
 input_config = load_yaml(f'{Path().absolute()}/pipeline/second-step-config.yml')
 
@@ -20,18 +22,79 @@ dep_output = ex_output['output'][0]['dependencies']
 dep_gt_dirty = ground_truth[0]['dependencies']
 dep_gt = clean_gt_dependencies(dep_gt_dirty)
 
-set_gt = set(frozenset(d.items()) for d in dep_gt)
-set_output = set(frozenset(d.items()) for d in dep_output)
+set_gt = set(
+    frozenset((key, value) for key, value in d.items() if is_a_valid_role_dependency(key))
+    for d in dep_gt)
+set_output = set(
+    frozenset((key, remove_explicit_tables_to_output(value))
+              for key, value in d.items())
+    for d in dep_output)
 
-# Find intersection
-common_dicts = set_gt & set_output
+tp = set_gt & set_output
+fn = set_gt - tp
+fp = set_output - tp
 
-tp = len(common_dicts)
-fn = len(set_gt) - len(common_dicts)
-fp = len(set_output) - len(common_dicts)
+tp_list = [dict(fs) for fs in tp]
+fn_list = [dict(fs) for fs in fn]
+fp_list = [dict(fs) for fs in fp]
 
-print(f"TP: {tp}\nFN: {fn}\nFP: {fp}")
+# print(f'TP: {tp_list}\n\nFN: {fn_list}\n\nFP: {fp_list}')
 
-# Optionally, convert frozensets back to dictionaries to see the common elements
-common_dicts_list = [dict(fs) for fs in common_dicts]
-print("Common dictionaries:", common_dicts_list)
+tp_count = len(tp)
+fn_count = len(fn)
+fp_count = len(fp)
+
+# print(f"TP: {tp_count}\nFN: {fn_count}\nFP: {fp_count}")
+
+# Visualization
+
+# Create a directed graph
+G = nx.DiGraph()
+
+tp_color, fn_color, fp_color = 'green', 'grey', 'red'
+
+# Add nodes and edges from the dictionaries
+for d in tp_list:
+    dep_dict = dict()
+    for key, value in d.items():
+        dep_dict[key] = value
+        G.add_node(value)
+    G.add_edge(dep_dict['from'], dep_dict['to'], color=tp_color)
+
+for d in fn_list:
+    dep_dict = dict()
+    for key, value in d.items():
+        dep_dict[key] = value
+        G.add_node(value)
+    G.add_edge(dep_dict['from'], dep_dict['to'], color=fn_color)
+
+for d in fp_list:
+    dep_dict = dict()
+    for key, value in d.items():
+        dep_dict[key] = value
+        G.add_node(value)
+    G.add_edge(dep_dict['from'], dep_dict['to'], color=fp_color)
+
+
+# Draw the graph
+pos = nx.spring_layout(G, seed=42)  # positions for all nodes
+plt.figure(figsize=(10, 8))
+
+edges = G.edges()
+colors = [G[u][v]['color'] for u,v in edges]
+
+# Draw nodes and edges
+nx.draw(G, pos, edge_color=colors, with_labels=True, node_color='lightblue', node_size=1000,
+        font_size=8, font_weight='bold', arrows=True)
+
+# Draw edge labels
+edge_labels = nx.get_edge_attributes(G, 'label')
+nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color='red')
+
+# TODO not working
+plt.text(0.5, 1.05, f'TP: {tp_count}\nFN: {fn_count}\nFP: {fp_count}', horizontalalignment='center',
+         fontsize=14, transform=plt.gca().transAxes)
+
+# Display the graph
+plt.title("Graph Visualization")
+plt.show()
--- src/main/python/pipeline/ssutils.py
+++ src/main/python/pipeline/ssutils.py
@@ -8,3 +8,17 @@ def clean_gt_dependencies(deps):
             combined_dict.update(sub_item)
         transformed.append(combined_dict)
     return transformed
+
+
+def remove_explicit_tables_to_output(dependency_value):
+    def remove_first_part(input_string):
+        if '.' in input_string:
+            position = input_string.find('.')
+            return input_string[position + 1:]
+        return input_string
+
+    return ''.join([remove_first_part(word) for word in dependency_value.split(' ')])
+
+
+def is_a_valid_role_dependency(dependency_key):
+    return dependency_key in ['from', 'to']
