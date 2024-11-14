de410be0a5b8f0ae9281f5e1caa2b3be302abd78
Enrico Gallinucci <enrico.gallinucci@unibo.it>
2024-11-14 12:35:33 +0100

    added run-all.sh script

---
+++ llm4dfm/run-all.sh
@@ -0,0 +1,10 @@
+./resources/automatic-run.sh 5 sql rq2-base gpt "1 2 3 4 5 6 7 8 9" gpt4o paper
+./resources/automatic-run.sh 5 sql rq2-example gpt "1 2 3 4 5 6 7 8 9" gpt4o paper
+# ./resources/automatic-run.sh 3 sql rq3-alg-base gpt "1 2 3 4 5 6 7 8 9" gpt4o paper
+./resources/automatic-run.sh 5 sql rq3-alg-example gpt "1 2 3 4 5 6 7 8 9" gpt4o paper
+# ./resources/automatic-run.sh 3 sql rq3-dec-base gpt "1 2 3 4 5 6 7 8 9" gpt4o paper
+./resources/automatic-run.sh 5 sql rq3-dec-example gpt "1 2 3 4 5 6 7 8 9" gpt4o paper
+./resources/automatic-run.sh 5 demand rq4-base gpt "1 2 3 4 5 6 7 8 9" gpt4o paper
+./resources/automatic-run.sh 5 demand rq4-example gpt "1 2 3 4 5 6 7 8 9" gpt4o paper
+# ./resources/automatic-run.sh 3 demand rq5-base gpt "1 2 3 4 5 6 7 8 9" gpt4o paper
+./resources/automatic-run.sh 5 demand rq5-example gpt "1 2 3 4 5 6 7 8 9" gpt4o paper
