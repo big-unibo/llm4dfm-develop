830ae9c0912b968e539b6b214fe1c0782fb95e6e
Luca1313 <luca.rubboli@libero.it>
2025-07-28 16:11:41 +0200

    chore: add results for gpt

---
+++ llm4dfm/run-api.sh
@@ -0,0 +1,5 @@
+./resources/automatic-run.sh 5 demand rq3-dec gpt api gpt "1 2 3 4 5 6 7 8 9" comparison
+./resources/automatic-run.sh 5 demand rq5 gpt api gpt "1 2 3 4 5 6 7 8 9" comparison
+
+./resources/automatic-run.sh 5 sql rq3-dec gpt api gpt "1 2 3 4 5 6 7 8 9" comparison
+./resources/automatic-run.sh 5 sql rq5 gpt api gpt "1 2 3 4 5 6 7 8 9" comparison
