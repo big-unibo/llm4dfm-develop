0e4b03a1e81e34ef56e33c23b8e026acf4f71f8d
Luca Rubboli <luca.rubboli@libero.it>
2025-08-23 12:10:34 +0200

    minor: slight change installation

--- setup-container.sh
+++ setup-container.sh
@@ -3,6 +3,6 @@
 apt install tmux
 poetry install
 pip install -U "huggingface_hub[cli]"
-apt-get install jq
+apt-get update && apt-get install jq
 HUGGINGFACE_TOKEN=$(yq -r '.hf.key.import' ./llm4dfm/resources/credentials.yml)
 hf auth login --token "$HUGGINGFACE_TOKEN"
